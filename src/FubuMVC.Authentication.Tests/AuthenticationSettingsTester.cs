49aeca6c55f1065447e4be3392b4d3b0d127beab
jeremydmiller <jeremydmiller@yahoo.com>
2012-11-29 08:48:20 -0600

    added the new exclusion technique to AuthenticationSettings

---
+++ src/FubuMVC.Authentication.Tests/AuthenticationSettingsTester.cs
@@ -0,0 +1,73 @@
+﻿using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
+using HtmlTags;
+using NUnit.Framework;
+using FubuCore.Reflection;
+using FubuTestingSupport;
+
+namespace FubuMVC.Authentication.Tests
+{
+    [TestFixture]
+    public class AuthenticationSettingsTester
+    {
+        [Test]
+        public void has_to_be_application_level()
+        {
+            typeof(AuthenticationSettings).HasAttribute<ApplicationLevelAttribute>()
+                .ShouldBeTrue();
+        }
+
+        [Test]
+        public void excludes_is_always_false_with_no_exclusions()
+        {
+            var settings = new AuthenticationSettings();
+            settings.ShouldBeExcluded(new BehaviorChain()).ShouldBeFalse();
+        }
+
+        [Test]
+        public void automatically_excludes_the_NotAuthenticated_attribute()
+        {
+            var chain = new BehaviorChain();
+            chain.AddToEnd(ActionCall.For<AuthenticatedEndpoints>(x => x.get_notauthenticated()));
+
+            new AuthenticationSettings().ShouldBeExcluded(chain)
+                .ShouldBeTrue();
+        }
+
+        [Test]
+        public void apply_a_custom_exclusion()
+        {
+            var chain = new BehaviorChain();
+            chain.AddToEnd(ActionCall.For<AuthenticatedEndpoints>(x => x.get_tag()));
+
+
+            var settings = new AuthenticationSettings();
+
+            settings.ShouldBeExcluded(chain).ShouldBeFalse();
+
+            settings.ExcludeChains.ResourceTypeIs<HtmlTag>();
+
+            settings.ShouldBeExcluded(chain).ShouldBeTrue();
+
+        }
+    }
+
+    public class AuthenticatedEndpoints
+    {
+        [NotAuthenticated]
+        public string get_notauthenticated()
+        {
+            return "anything";
+        }
+        
+        public string get_authenticated()
+        {
+            return "else";
+        }
+
+        public HtmlTag get_tag()
+        {
+            return new HtmlTag("div");
+        }
+    }
+}
--- src/FubuMVC.Authentication/AuthenticationSettings.cs
+++ src/FubuMVC.Authentication/AuthenticationSettings.cs
@@ -1,14 +1,38 @@
+using System;
+using System.Collections.Generic;
+using FubuCore.Descriptions;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
+using FubuMVC.Core.Registration.Policies;
+using System.Linq;
+using FubuCore;
+
 namespace FubuMVC.Authentication
 {
+    [ApplicationLevel]
     public class AuthenticationSettings
     {
+        private readonly ChainPredicate _exclusions = new ChainPredicate();
+
         public AuthenticationSettings()
         {
+            _exclusions.Matching<NotAuthenticatedFilter>();
+
             ExpireInMinutes = 180;
             SlidingExpiration = true;
 
             MaximumNumberOfFailedAttempts = 3;
             CooloffPeriodInMinutes = 60;
+        }
+
+        public ChainPredicate ExcludeChains
+        {
+            get { return _exclusions; }
+        }
+
+        public bool ShouldBeExcluded(BehaviorChain chain)
+        {
+            return _exclusions.As<IChainFilter>().Matches(chain);
         }
 
         public bool SlidingExpiration { get; set; }
@@ -20,4 +44,14 @@ namespace FubuMVC.Authentication
         // *should* only be for testing
         public bool NeverExpires { get; set; }
     }
+
+    [Title("Any action with the [NotAuthenticated] attribute")]
+    public class NotAuthenticatedFilter : IChainFilter
+    {
+        public bool Matches(BehaviorChain chain)
+        {
+            return chain.Calls.Any(x => x.HasAttribute<NotAuthenticatedAttribute>());
+        }
+    }
+    
 }
