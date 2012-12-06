d2549f4b7504b51d98e6803bfdff891edc1829fb
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-06 09:24:39 -0600

    starting the new FubuMVC.PersistedMembership assembly

---
+++ src/FubuMVC.PersistedMembership.Testing/PasswordHash_is_predictable.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuMVC.PersistedMembership.Testing
+{
+    [TestFixture]
+    public class PasswordHash_is_predictable
+    {
+        [Test]
+        public void see_it_in_action()
+        {
+            var hash = new PasswordHash();
+            var password = "something";
+
+            for (int i = 0; i < 50; i++)
+            {
+                Debug.WriteLine(hash.CreateHash(password));
+            }
+
+                hash.CreateHash(password).ShouldEqual(hash.CreateHash(password));
+            hash.CreateHash(password).ShouldEqual(hash.CreateHash(password));
+            hash.CreateHash(password).ShouldEqual(hash.CreateHash(password));
+            hash.CreateHash(password).ShouldEqual(hash.CreateHash(password));
+
+            hash.CreateHash(password).ShouldNotEqual(password);
+        }
+    }
+}
---
+++ src/FubuMVC.PersistedMembership/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FubuMVC.Authentication;
+using FubuMVC.Authentication.Membership;
+using FubuMVC.Core;
+using FubuPersistence;
+using System.Linq;
+
+namespace FubuMVC.PersistedMembership
+{
+    public class User : UserInfo, IEntity
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class PersistedMembership<T> : IFubuRegistryExtension where T : User
+    {
+        public void Configure(FubuRegistry registry)
+        {
+            registry.Services(x => {
+                x.ReplaceService<IMembershipRepository, MembershipRepository<T>>();
+                x.SetServiceIfNone<IPasswordHash, PasswordHash>();
+            });
+
+            registry.AlterSettings<AuthenticationSettings>(x => {
+                x.Strategies.AddToEnd(new MembershipNode(typeof(MembershipRepository<T>)));
+            });
+        }
+    }
+
+    public class MembershipRepository<T> : IMembershipRepository where T : User
+    {
+        private readonly IEntityRepository _repository;
+        private readonly IPasswordHash _hash;
+
+        public MembershipRepository(IEntityRepository repository, IPasswordHash hash)
+        {
+            _repository = repository;
+            _hash = hash;
+        }
+
+        public bool MatchesCredentials(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUserInfo FindByName(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
